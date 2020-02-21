e6bdbb96551c8a0b078077f59acdb069b9c00dd4
Ethan Butt <ethanbutt309@gmail.com>
2020-02-21 16:37:29 -0600

    Created basic scene warp script.

---
+++ Assets/Scripts/Warp.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Warp : MonoBehaviour
+{
+    string[] minigames = {"Observatory", "Stardust", "Copycat"};
+    const string BASE = minigames[0];
+    string focus = BASE;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(focus != BASE)
+        {
+            print("Loading scene: " + focus);
+            SceneManager.LoadScene(focus);
+        }
+    }
+}
