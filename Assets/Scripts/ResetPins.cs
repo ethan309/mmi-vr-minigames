24a511e4dd87536c98f5220b8ad9d5260b565d47
Ethan Butt <ethanbutt309@gmail.com>
2020-02-21 15:45:35 -0600

    Added scripts for snapping objected to place based on trigger.

---
+++ Assets/Scripts/ResetPins.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+using TMPro;
+using System.Linq;
+
+public class ResetPins : MonoBehaviour
+{
+    private GameObject[] marbles;
+
+    private int winningScore;
+    private int score;
+
+    public TextMeshProUGUI playerScoreDisplayText;
+    // public Text endgameMessage;
+
+    List<Vector2> GenerateRingPoints(int marbleCount) {
+        List<Vector2> points = new List<Vector2>();
+        while(points.Count < marbleCount) {
+            const int ringSize = 5;
+            const float minSeparation = 0.5F;
+            Vector2 newPoint = UnityEngine.Random.insideUnitCircle * ringSize;
+            if(!points.Any(point => Vector3.Distance(point, newPoint) < minSeparation))
+            {
+                points.Add(newPoint);
+            }
+        }
+        return points;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        marbles = GameObject.FindGameObjectsWithTag("Standing Marble");
+        winningScore = marbles.Count();
+        //endgameMessage.text = "";
+    }
+
+    void UpdateScore(int newScore)
+    {
+        score = newScore;
+        playerScoreDisplayText.text = "Score: " + score.ToString() + "/" + winningScore.ToString();
+        if (score >= winningScore)
+        {
+            playerScoreDisplayText.text = "Score: " + score.ToString() + "/" + winningScore.ToString() + "\n" + "GAME OVER. PLEASE RESET MARBLES.";
+        }
+        else
+        {
+            //print("NEW SCORE: " + score);
+            //endgameMessage.text = "";
+        }
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            score = 0;
+            foreach (GameObject marble in marbles)
+            {
+                marble.GetComponent<Resetable>().ToggleReset(true);
+            }
+            List<Vector2> newPoints = GenerateRingPoints(marbles.Count());
+            for (int i = 0; i < marbles.Count(); i++)
+            {
+                marbles[i].GetComponent<Resetable>().TriggerRearrange(newPoints[i].x, newPoints[i].y);
+            }
+        }
+    } 
+
+    private void FixedUpdate()
+    {
+        int newScore = 0;
+        foreach (GameObject marble in marbles)
+        {
+            if (marble.GetComponent<Resetable>().hit)
+            {
+                newScore += 1;
+            }
+        }
+        UpdateScore(newScore);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            score = 0;
+            foreach (GameObject marble in marbles)
+            {
+                marble.GetComponent<Resetable>().ToggleReset(true);
+            }
+            List<Vector2> newPoints = GenerateRingPoints(marbles.Count());
+            for (int i = 0; i < marbles.Count(); i++)
+            {
+                marbles[i].GetComponent<Resetable>().TriggerRearrange(newPoints[i].x, newPoints[i].y);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SnapToPoint.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Valve.VR.InteractionSystem.Sample
+{
+    public class SnapToPoint : MonoBehaviour, Resetable
+    {
+        private bool shouldReset;
+        private GameObject centerpoint;
+        private Vector3 snapTo;
+        private Quaternion snapAngle;
+        private Rigidbody body;
+        public float snapTime = 2;
+
+        private float dropTimer;
+        private Interactable interactable;
+
+        bool Resetable.hit => !transform.gameObject.activeSelf;
+
+        private void RecordSnapPosition()
+        {
+            RecordSnapPosition(transform.position, transform.rotation);
+        }
+
+        private void RecordSnapPosition(Vector3 position, Quaternion rotation)
+        {
+            snapTo = position;
+            snapAngle = rotation;
+        }
+
+        public void ToggleReset(bool status)
+        {
+            shouldReset = status;
+            if(status)
+            {
+                transform.gameObject.SetActive(true);
+            }
+        }
+
+        public void TriggerRearrange(float newX, float newZ)
+        {
+            Vector3 newPosition = new Vector3(newX, 0.3F, newZ);
+            // transform.SetPositionAndRotation(newPosition, transform.rotation);
+            RecordSnapPosition(newPosition, transform.rotation);
+            ToggleReset(true);
+        }
+
+        private void Start()
+        {
+            RecordSnapPosition();
+            centerpoint = GameObject.FindGameObjectsWithTag("Centerpoint")[0];
+            shouldReset = false;
+            interactable = GetComponent<Interactable>();
+            body = GetComponent<Rigidbody>();
+        }
+
+        private void FixedUpdate()
+        {
+            bool used = false;
+
+            if (interactable != null)
+                used = interactable.attachedToHand;
+
+            if (used || transform.position == snapTo && transform.rotation == snapAngle)
+            {
+                body.velocity = Vector3.zero;
+                body.angularVelocity = Vector3.zero;
+                body.isKinematic = false;
+                dropTimer = -1;
+                ToggleReset(false);
+            }
+            else if (shouldReset)
+            {
+                dropTimer += Time.deltaTime / (snapTime / 2);
+
+                body.isKinematic = dropTimer > 1;
+
+                // Already back in target position
+                if (dropTimer > 1)
+                {
+                    //transform.parent = snapTo;
+                    transform.position = snapTo;
+                    transform.rotation = snapAngle;
+                    ToggleReset(false);
+                    dropTimer = -1;
+                }
+                // Still returning to target position
+                else
+                {
+                    float t = Mathf.Pow(35, dropTimer);
+
+                    body.velocity = Vector3.Lerp(body.velocity, Vector3.zero, Time.fixedDeltaTime * 4);
+                    if (body.useGravity)
+                        body.AddForce(-Physics.gravity);
+
+                    transform.position = Vector3.Lerp(transform.position, snapTo, Time.fixedDeltaTime * t * 3);
+                    transform.rotation = Quaternion.Slerp(transform.rotation, snapAngle, Time.fixedDeltaTime * t * 2);
+                }
+            }
+            else
+            {
+                float separation = Vector3.Distance(centerpoint.transform.position, transform.position);
+                if(separation > 6.1)
+                {
+                    transform.gameObject.SetActive(false);
+                }
+            }
+        }
+    }
+}
