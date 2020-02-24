bb000ddfa7ce2130ddcfcc3421f7b984664d93a1
Conner Hallett <35943383+challett3@users.noreply.github.com>
2020-02-24 15:51:52 -0600

    Temproray commit for ZoomCrank implementation.

---
+++ Assets/Scripts/ZoomCrank.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Valve.VR.InteractionSystem;
+
+public class ZoomCrank : MonoBehaviour
+{
+
+    public GameObject targetCamera;
+    public GameObject targetCrank;
+    private int axisOfRotation;
+    public float rotation;
+    public int cycles;
+    private float oldRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CircularDrive circularDrive = targetCrank.GetComponent <CircularDrive> ();
+        axisOfRotation = (int) circularDrive.axisOfRotation;
+        rotation = targetCrank.transform.rotation.eulerAngles[axisOfRotation];
+        cycles = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        oldRotation = rotation;
+        rotation = targetCrank.transform.rotation.eulerAngles[axisOfRotation];
+        CheckCycleIncrement();
+        CheckCycleDecrement();
+
+    }
+
+    void CheckCycleIncrement() {
+        if (rotation > 0 && rotation < 120 && oldRotation < 360 && oldRotation > 240) {
+            cycles++;
+        }
+    }
+
+    void CheckCycleDecrement() {
+        if (rotation > 240 && rotation < 360 && oldRotation < 120 && oldRotation > 0) {
+            cycles--;
+        }
+    }
+
+    void ZoomCamera() {
+
+    }
+}
