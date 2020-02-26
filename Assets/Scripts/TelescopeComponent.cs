39df929cf42a50669ab1969205a7464386175ba8
Ryan Le <ryanle@live.com>
2020-02-26 17:02:06 -0600

    Testing transparency building

--- Assets/Scripts/TelescopeComponent.cs
+++ Assets/Scripts/TelescopeComponent.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 public class TelescopeComponent : MonoBehaviour
 {
+    public float transparency = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Renderer renderer = gameObject.GetComponent<Renderer>();
+        Color materialColor = renderer.material.color;
+        renderer.material.color = new Color(materialColor.r, materialColor.g, materialColor.b, transparency);
+    }
 
     void OnCollisionEnter(Collision collision)
     {
+        
         // The collision object is what the Component collides with
-        if (collision.gameObject.CompareTag("Telescope Component"))
+        if (collision.gameObject.CompareTag("Guide"))
         {
             // When the component is 'placed', it should be destroyed and should change the color of the telescope itself to opaque
             Destroy(gameObject);
