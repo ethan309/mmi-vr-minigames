bd071d8264fa6e563fe6d96843d6ad9b767feaec
Ethan Butt <ethanbutt309@gmail.com>
2020-02-28 17:26:20 -0600

    Linking materials to scripts for telescope component transparency.

--- Assets/Scripts/TelescopeComponent.cs
+++ Assets/Scripts/TelescopeComponent.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class TelescopeComponent : MonoBehaviour, Guideable
 {
-    public string opaqueMaterialName;
-    public string transparentMaterialName;
+    public Material opaqueMaterial;
+    public Material transparentMaterial;
 
     // Start is called before the first frame update
     void Start()
@@ -14,14 +14,13 @@ public class TelescopeComponent : MonoBehaviour, Guideable
     }
 
     public void PlaceOpaque() {
-        Material newMat = Resources.Load("Materials/" + opaqueMaterialName, typeof(Material)) as Material;
+        //Material newMat = Resources.Load("Materials/" + opaqueMaterialName, typeof(Material)) as Material;
         Renderer renderer = gameObject.GetComponent<Renderer>();
-        renderer.material = newMat;
+        renderer.material = opaqueMaterial;
     }
 
     public void PlaceTransparent() {
-        Material newMat = Resources.Load("Materials/" + transparentMaterialName, typeof(Material)) as Material;
         Renderer renderer = gameObject.GetComponent<Renderer>();
-        renderer.material = newMat;
+        renderer.material = transparentMaterial;
     }
 }
