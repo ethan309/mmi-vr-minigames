93b27486df3c363650b5b81b64bc24f92ba18033
Conner Hallett <35943383+challett3@users.noreply.github.com>
2020-02-28 17:00:40 -0600

    Added Telescope Component Parent script.

---
+++ Assets/Scripts/TelescopeComponentParent.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TelescopeComponentParent : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        // The collision object is what the Component collides with
+        if (collision.gameObject.CompareTag("Telescope Pickup Component"))
+        {
+            // When the component is 'placed', it should be destroyed and should change the color of the telescope itself to opaque
+            Destroy(gameObject);
+            foreach (Transform child in transform) {
+                child.PlaceOpaque();
+            }
+        }
+    }
+
+    void Update()
+    {
+        // The collision object is what the Component collides with
+        if (input.GetKeyUp(KeyCode.R))
+        {
+            // When the component is 'placed', it should be destroyed and should change the color of the telescope itself to opaque
+            foreach (Transform child in transform) {
+                child.PlaceOpaque();
+            }
+        }
+    }
+}
