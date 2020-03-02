de3c18487d9bd11a91d486e7a9b1b5ba0ec16caa
Ryan Le <ryanle@live.com>
2020-03-02 17:15:27 -0600

    Added code to show the lens on successful building

---
+++ Assets/Scripts/Telescope.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Telescope : MonoBehaviour
+{
+    public int placed = 0;
+    public GameObject lens;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (Transform child in transform) 
+        {
+            if (child.getChild(0).placed) {
+                placed += 1;
+            }
+
+            if (placed >= 3) {
+                lens.transform.gameObject.SetActive(true);
+            }
+        }
+    }
+}
