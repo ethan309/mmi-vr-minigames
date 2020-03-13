f643fe26a8279061d4ba8ba613b4caac53a23e5a
Ryan Le <ryanle@live.com>
2020-03-13 14:24:32 -0500

    Started basic development of Stardust Collection

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    public int collected;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        // Test Tube is a placeholder name, replace it with whatever the tube/vacuum/light in hand ends up being
+        if (collision.gameObject.CompareTag("Test Tube"))
+        {
+            Destroy(gameObject);
+            collected += 1;
+            
+        }
+    }
+}
