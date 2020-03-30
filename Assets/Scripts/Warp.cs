2e8ebf6e1d0eac7d0a24a1486ec91026244375e9
Conner Hallett <35943383+challett3@users.noreply.github.com>
2020-03-30 14:10:18 -0500

    Warp.cs now grabs the target of the warp through raycasting.

--- Assets/Scripts/Warp.cs
+++ Assets/Scripts/Warp.cs
@@ -6,19 +6,29 @@ using Valve.VR;
 
 public class Warp : MonoBehaviour
 {
-    string[] minigames = {"Observatory", "Stardust", "Copycat"};
+    public string target;
+    public bool onlyObservatory;
+
     string BASE;
     string focus;
+    private GameObject raycaster;
 
     void Start()
     {
         BASE = SceneManager.GetActiveScene().name;
         focus = BASE;
+        raycaster = GameObject.Find("Telescope Raycaster");
     }
     
     // Warp to minigame (new scene). 'W' key used for manual debugging.
     void Update()
     {
+        if (onlyObservatory) {
+            target = "Observatory"
+        } else {
+            target = raycaster.GetComponent<TelescopeRaycaster>().warpTag;
+        }
+
         if(Input.GetKeyUp(KeyCode.W))
         {
             if(focus != null && focus != BASE)
