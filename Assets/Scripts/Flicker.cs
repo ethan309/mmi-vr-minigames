47ef525cbe6c1d74ec1416bbbace80c7898aecaf
Ryan Le <ryanle@live.com>
2020-04-15 21:40:25 -0500

    Added fast (functional) starlight flicker

---
+++ Assets/Scripts/Flicker.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flicker : MonoBehaviour
+{
+    public float maxRange;
+    public float minRange;
+
+    private Light lightSource;
+
+    void Start()
+    {
+        minRange = 1.0f;
+        maxRange = 5.0f;
+        lightSource = GetComponent<Light>();
+    }
+
+    void Update()
+    {
+        if (lightSource.range >= maxRange) StartCoroutine(DecreaseRange(lightSource));
+        else if (lightSource.range <= minRange) StartCoroutine(IncreaseRange(lightSource));
+    }
+
+    // IEnumerator DoFlicker()
+    // {
+    //     if (lightSource.range <= minRange)
+    //     {
+    //         yield return StartCoroutine(IncreaseRange(lightSource));
+    //     }
+
+    //     if (lightSource.range >= maxRange) 
+    //     {
+    //         yield return StartCoroutine(DecreaseRange(lightSource));
+    //     }
+    //     print(lightSource.range);
+    // }
+
+    IEnumerator IncreaseRange(Light source)
+    {
+        while (source.range <= maxRange)
+        {
+            source.range += 0.5f;
+            yield return source;
+        }
+    }
+
+    IEnumerator DecreaseRange(Light source)
+    {
+        while (source.range >= minRange)
+        {
+            source.range -= 0.5f;
+            yield return source;
+        }
+    }
+}
