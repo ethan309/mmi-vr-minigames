5d51456b2576fbe0666dc6de7873c5963f3ba2c6
Ryan Le <ryanle@live.com>
2020-04-16 19:59:55 -0500

    Added vertical pacer script for stardust movement

--- Assets/Scripts/Flicker.cs
+++ Assets/Scripts/Flicker.cs
@@ -1,34 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class Flicker : MonoBehaviour
 {
-    private const float MAX_INTENSITY = 5.0f;
+    private const float MAX_INTENSITY = 4.0f;
     private const float MIN_INTENSITY = 1.25f;
-    private const float DELTA = 0.05f;
+    private const float DELTA = 0.03f;
     private const float LIGHTING_CHANGE_DELAY = 0.01f;
-
     private Light lightSource;
-
     void Start()
     {
         lightSource = GetComponent<Light>();
         StartCoroutine(UpdateStardustGlowIntensity(lightSource));
     }
-
     IEnumerator UpdateStardustGlowIntensity(Light source)
     {
         float delta = DELTA;
-        while(true)
+        while (true)
         {
             if(source.range > MAX_INTENSITY)
                 delta = -DELTA;
             else if(source.range < MIN_INTENSITY)
                 delta = DELTA;
-
             source.range += delta;
-            
             yield return new WaitForSeconds(LIGHTING_CHANGE_DELAY);
         }
     }
---
+++ Assets/Scripts/VerticalPacer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class VerticalPacer : MonoBehaviour
+{
+    public float range;
+    private float minHeight;
+    private float maxHeight;
+    private const float POSITON_CHANGE_DELAY = 0.01f;
+    private const float DELTA = 0.04f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // print("Current position: " + transform.position.y);
+        minHeight = transform.position.y;
+        maxHeight = transform.position.y + range;
+        // print("Min Height: " + minHeight);
+        // print("Max Height: " + maxHeight);
+        StartCoroutine(UpdateVerticalPosition());
+    }
+    IEnumerator UpdateVerticalPosition()
+    {
+        float delta = DELTA;
+        float height = minHeight;
+        while (true) 
+        {
+            if (transform.position.y > maxHeight)
+                delta = -DELTA;
+            else if (transform.position.y < minHeight)
+                delta = DELTA;
+            height += delta;
+            transform.position = new Vector3(transform.position.x, height, transform.position.z);
+            yield return new WaitForSeconds(POSITON_CHANGE_DELAY);
+        }
+    }
+}
